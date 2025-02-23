c01cd994372e95311612de416a5aef165fed637e
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-02-23 13:44:06 +0530

    Data modelling in appwrite

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-riverflow"
+export const questionCollection = "question"
+export const answerCollection = "answers"
+export const commentCollection = "comment"
+export const voteCollection = "votes"
+export const questionAttachentBucket = "question-attachment"
